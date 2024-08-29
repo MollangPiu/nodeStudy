9f5da7a69dd7449f9557bcd8bb88545f2bd7c0c8
MHS <hayaruby7909@gmail.com>
2024-08-29 16:14:29 +0900

    Feat: cors적용

--- study.js
+++ study.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 
 app.listen(3000, function () {
     console.log('start');
 })
+
+//CORS 적용
+app.use(cors());
+
 
 
 app.get('/', (req, res) => { res.send('Hello World'); })
